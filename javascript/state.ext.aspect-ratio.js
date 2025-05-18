b9ab976057be3d4580146ed09e9556b60aae1330
Kevin <kevintan.me@gmail.com>
2025-05-18 18:47:53 +0700

    Increase loading delay for Aspect Ratio extension to 10 seconds

--- javascript/state.ext.aspect-ratio.js
+++ javascript/state.ext.aspect-ratio.js
@@ -31,7 +31,7 @@ state.extensions['aspect-ratio'] = (function () {
         setTimeout(function () {
             handleSelects();
             console.log('Aspect Ratio extension loaded');
-        }, 8000);
+        }, 10000);
     }
 
     function init() {
